651896ffd9900ab5c11735098ff50936ed54e650
zzggxx09-byte <zzggxx09@gmail.com>
2026-07-29 20:16:37 +0300

    Update ClientModelEvents.java

--- src/main/java/com/anyamod/proxy/ClientModelEvents.java
+++ src/main/java/com/anyamod/proxy/ClientModelEvents.java
@@ -1,25 +1,28 @@
 package com.anyamod.proxy;
 
 import com.anyamod.AnyaMod;
+import com.anyamod.entity.EntityAnya;
 import com.anyamod.init.ModItems;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 
-/**
- * Реєстрація моделі яйця має відбутись САМЕ на ModelRegistryEvent.
- */
 @Mod.EventBusSubscriber(modid = AnyaMod.MODID, value = Side.CLIENT)
 public class ClientModelEvents {
 
     @SubscribeEvent
     public static void onModelRegistry(ModelRegistryEvent event) {
+        // 1. Модель яйця
         ModelLoader.setCustomModelResourceLocation(
                 ModItems.ANYA_SPAWN_EGG, 0,
                 new ModelResourceLocation(AnyaMod.MODID + ":anya_spawn_egg", "inventory")
         );
+
+        // 2. Рендер моба (Forge гарантовано підхопить його вчасно!)
+        RenderingRegistry.registerEntityRenderingHandler(EntityAnya.class, RenderAnya::new);
     }
 }
