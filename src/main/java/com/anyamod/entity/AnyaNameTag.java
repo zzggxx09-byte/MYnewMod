c67bd88029ab8150a8368c9952a9f76b1ca542b7
zzggxx09-byte <zzggxx09@gmail.com>
2026-07-29 09:26:32 +0300

    Create AnyaNameTag.java

---
+++ src/main/java/com/anyamod/entity/AnyaNameTag.java
@@ -0,0 +1,15 @@
+package com.anyamod.entity;
+
+import net.minecraft.util.text.TextFormatting;
+
+/**
+ * Окремий файл лише для ніка Anya.
+ * Міняти текст/колір ніка - тільки тут, EntityAnya цього файлу не торкається.
+ */
+public final class AnyaNameTag {
+
+    public static final String NAME = TextFormatting.AQUA + "Anya";
+
+    private AnyaNameTag() {
+    }
+}
