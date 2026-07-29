4ab8b17e8844133233a12bdce4755aa8ddde884a
zzggxx09-byte <zzggxx09@gmail.com>
2026-07-29 20:25:31 +0300

    Update AnyaMod.java

--- src/main/java/com/anyamod/AnyaMod.java
+++ src/main/java/com/anyamod/AnyaMod.java
@@ -5,7 +5,7 @@ import net.minecraft.creativetab.CreativeTabs;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent; // <-- імпортуємо PreInit
 
 @Mod(modid = AnyaMod.MODID, name = AnyaMod.NAME, version = AnyaMod.VERSION)
 public class AnyaMod {
@@ -14,7 +14,6 @@ public class AnyaMod {
     public static final String NAME = "Anya Mod";
     public static final String VERSION = "1.0.0";
 
-    // Окрема креативна вкладка, щоб яйце спавну Anya було легко знайти в інвентарі/творчому режимі
     public static final CreativeTabs TAB_ANYA = new CreativeTabs("anyamodTab") {
         @Override
         public net.minecraft.item.ItemStack getTabIconItem() {
@@ -25,8 +24,9 @@ public class AnyaMod {
     @SidedProxy(clientSide = "com.anyamod.proxy.ClientProxy", serverSide = "com.anyamod.proxy.CommonProxy")
     public static CommonProxy proxy;
 
+    // ЗМІНЕНО: перенесено з init в preInit
     @EventHandler
-    public void init(FMLInitializationEvent event) {
+    public void preInit(FMLPreInitializationEvent event) {
         proxy.registerRenderers();
     }
 }
