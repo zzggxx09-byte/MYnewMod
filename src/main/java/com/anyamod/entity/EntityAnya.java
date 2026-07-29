a2c84bdd2ad2baef3e03bf6c0bc3d1ee60bfdee4
zzggxx09-byte <zzggxx09@gmail.com>
2026-07-29 08:54:20 +0300

    Create EntityAnya.java

---
+++ src/main/java/com/anyamod/entity/EntityAnya.java
@@ -0,0 +1,51 @@
+package com.anyamod.entity;
+
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
+import net.minecraft.entity.passive.EntityVillager;
+
+/**
+ * ЗАГОТОВКА моба "Anya".
+ * Поки що це просто житель (модель/анімації/AI успадковані від EntityVillager),
+ * з примусовим ніком "Anya" над головою.
+ *
+ * У майбутньому:
+ *  - замінити super-клас або рендер на кастомну модель
+ *  - прибрати/змінити торгівлю (зараз вимкнена через processInteract)
+ *  - додати власний AI/діалоги
+ */
+public class EntityAnya extends EntityVillager {
+
+    public EntityAnya(World worldIn) {
+        super(worldIn);
+        this.setCustomNameTag(TextFormatting.AQUA + "Anya");
+        this.setAlwaysRenderNameTag(true);
+        // Прибираємо професію-специфічний вигляд (одяг), лишаємо базову модель жителя
+        this.setProfession(0);
+    }
+
+    @Override
+    protected boolean processInteract(EntityPlayer player, EnumHand hand) {
+        // Заготовка: правий клік нічого не робить (торгівля вимкнена),
+        // щоб не заважало майбутній кастомній логіці NPC.
+        return true;
+    }
+
+    @Override
+    public boolean canMate() {
+        return false;
+    }
+
+    @Override
+    public boolean canBreed() {
+        return false;
+    }
+
+    @Override
+    public EntityVillager createChild(EntityLivingBase ageable) {
+        return null;
+    }
+}
