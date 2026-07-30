87b12ea7b3c6e653b723dff2d75e2fcf11b3248c
zzggxx09-byte <zzggxx09@gmail.com>
2026-07-30 12:26:21 +0300

    Update ModelAnyaHD.java

--- src/main/java/com/anyamod/entity/ModelAnyaHD.java
+++ src/main/java/com/anyamod/entity/ModelAnyaHD.java
@@ -8,6 +8,7 @@ import net.minecraft.util.math.MathHelper;
 
 public class ModelAnyaHD extends ModelBase {
 
+    // Перший (базовий) шар
     public final ModelRenderer bipedHead;
     public final ModelRenderer bipedBody;
     public final ModelRenderer bipedRightArm;
@@ -15,10 +16,19 @@ public class ModelAnyaHD extends ModelBase {
     public final ModelRenderer bipedRightLeg;
     public final ModelRenderer bipedLeftLeg;
 
+    // Другий (overlay) шар - капелюх/куртка/рукави/штани
+    public final ModelRenderer bipedHeadwear;
+    public final ModelRenderer bipedBodyWear;
+    public final ModelRenderer bipedRightArmwear;
+    public final ModelRenderer bipedLeftArmwear;
+    public final ModelRenderer bipedRightLegwear;
+    public final ModelRenderer bipedLeftLegwear;
+
     public ModelAnyaHD() {
         this.textureWidth = 128;
         this.textureHeight = 128;
 
+        // ===== Базовий шар =====
         this.bipedHead = new ModelRenderer(this, 0, 0);
         this.bipedHead.addBox(-8.0F, -16.0F, -8.0F, 16, 16, 16, 0.0F);
         this.bipedHead.setRotationPoint(0.0F, 0.0F, 0.0F);
@@ -27,7 +37,6 @@ public class ModelAnyaHD extends ModelBase {
         this.bipedBody.addBox(-8.0F, 0.0F, -4.0F, 16, 24, 8, 0.0F);
         this.bipedBody.setRotationPoint(0.0F, 0.0F, 0.0F);
 
-        // Руки Alex (slim): ширина 6 (замість 8 у Steve), rotation point Y = 5.0 (замість 4.0)
         this.bipedRightArm = new ModelRenderer(this, 80, 32);
         this.bipedRightArm.addBox(-4.0F, -4.0F, -4.0F, 6, 24, 8, 0.0F);
         this.bipedRightArm.setRotationPoint(-10.0F, 5.0F, 0.0F);
@@ -45,22 +54,69 @@ public class ModelAnyaHD extends ModelBase {
         this.bipedLeftLeg.mirror = true;
         this.bipedLeftLeg.addBox(-4.0F, 0.0F, -4.0F, 8, 24, 8, 0.0F);
         this.bipedLeftLeg.setRotationPoint(3.8F, 24.0F, 0.0F);
+
+        // ===== Overlay шар (той самий розмір + невеликий "надув" назовні) =====
+        this.bipedHeadwear = new ModelRenderer(this, 64, 0);
+        this.bipedHeadwear.addBox(-8.0F, -16.0F, -8.0F, 16, 16, 16, 1.0F);
+        this.bipedHeadwear.setRotationPoint(0.0F, 0.0F, 0.0F);
+
+        this.bipedBodyWear = new ModelRenderer(this, 32, 64);
+        this.bipedBodyWear.addBox(-8.0F, 0.0F, -4.0F, 16, 24, 8, 0.5F);
+        this.bipedBodyWear.setRotationPoint(0.0F, 0.0F, 0.0F);
+
+        this.bipedRightArmwear = new ModelRenderer(this, 80, 64);
+        this.bipedRightArmwear.addBox(-4.0F, -4.0F, -4.0F, 6, 24, 8, 0.5F);
+        this.bipedRightArmwear.setRotationPoint(-10.0F, 5.0F, 0.0F);
+
+        this.bipedLeftArmwear = new ModelRenderer(this, 80, 64);
+        this.bipedLeftArmwear.mirror = true;
+        this.bipedLeftArmwear.addBox(-2.0F, -4.0F, -4.0F, 6, 24, 8, 0.5F);
+        this.bipedLeftArmwear.setRotationPoint(10.0F, 5.0F, 0.0F);
+
+        this.bipedRightLegwear = new ModelRenderer(this, 0, 64);
+        this.bipedRightLegwear.addBox(-4.0F, 0.0F, -4.0F, 8, 24, 8, 0.5F);
+        this.bipedRightLegwear.setRotationPoint(-3.8F, 24.0F, 0.0F);
+
+        this.bipedLeftLegwear = new ModelRenderer(this, 0, 64);
+        this.bipedLeftLegwear.mirror = true;
+        this.bipedLeftLegwear.addBox(-4.0F, 0.0F, -4.0F, 8, 24, 8, 0.5F);
+        this.bipedLeftLegwear.setRotationPoint(3.8F, 24.0F, 0.0F);
     }
 
     @Override
     public void render(Entity entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
         this.setRotationAngles(limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale, entityIn);
 
-        // Масштаб застосовуємо ТУТ, а не в preRenderCallback -
-        // так фіксований підйом моделі (-1.501) рушій виконує в повну силу.
+        // Overlay-частини мають рухатись синхронно з базовими (та сама рука/нога/голова)
+        this.bipedHeadwear.rotateAngleX = this.bipedHead.rotateAngleX;
+        this.bipedHeadwear.rotateAngleY = this.bipedHead.rotateAngleY;
+        this.bipedHeadwear.rotateAngleZ = this.bipedHead.rotateAngleZ;
+
+        this.bipedRightArmwear.rotateAngleX = this.bipedRightArm.rotateAngleX;
+        this.bipedRightArmwear.rotateAngleZ = this.bipedRightArm.rotateAngleZ;
+        this.bipedLeftArmwear.rotateAngleX = this.bipedLeftArm.rotateAngleX;
+        this.bipedLeftArmwear.rotateAngleZ = this.bipedLeftArm.rotateAngleZ;
+
+        this.bipedRightLegwear.rotateAngleX = this.bipedRightLeg.rotateAngleX;
+        this.bipedLeftLegwear.rotateAngleX = this.bipedLeftLeg.rotateAngleX;
+
         GlStateManager.pushMatrix();
         GlStateManager.scale(0.5F, 0.5F, 0.5F);
+
         this.bipedHead.render(scale);
         this.bipedBody.render(scale);
         this.bipedRightArm.render(scale);
         this.bipedLeftArm.render(scale);
         this.bipedRightLeg.render(scale);
         this.bipedLeftLeg.render(scale);
+
+        this.bipedHeadwear.render(scale);
+        this.bipedBodyWear.render(scale);
+        this.bipedRightArmwear.render(scale);
+        this.bipedLeftArmwear.render(scale);
+        this.bipedRightLegwear.render(scale);
+        this.bipedLeftLegwear.render(scale);
+
         GlStateManager.popMatrix();
     }
 
