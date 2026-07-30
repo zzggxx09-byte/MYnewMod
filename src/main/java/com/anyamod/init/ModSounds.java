047860cb8052920010e3e82f34bd2cd956ce4103
zzggxx09-byte <zzggxx09@gmail.com>
2026-07-30 10:56:52 +0300

    Update ModSounds.java

--- src/main/java/com/anyamod/init/ModSounds.java
+++ src/main/java/com/anyamod/init/ModSounds.java
@@ -6,19 +6,16 @@ import net.minecraft.util.SoundEvent;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
 
-@ObjectHolder(AnyaMod.MODID)
 @Mod.EventBusSubscriber(modid = AnyaMod.MODID)
 public class ModSounds {
 
-    // Створюємо самі SoundEvent об'єкти
-    public static final SoundEvent ANYA_AMBIENT = createSoundEvent("entity.anya.ambient");
-    public static final SoundEvent ANYA_HURT = createSoundEvent("entity.anya.hurt");
+    public static final SoundEvent ANYA_AMBIENT = createSound("entity.anya.ambient");
+    public static final SoundEvent ANYA_HURT = createSound("entity.anya.hurt");
 
-    private static SoundEvent createSoundEvent(String soundName) {
-        ResourceLocation location = new ResourceLocation(AnyaMod.MODID, soundName);
-        return new SoundEvent(location).setRegistryName(location);
+    private static SoundEvent createSound(String name) {
+        ResourceLocation loc = new ResourceLocation(AnyaMod.MODID, name);
+        return new SoundEvent(loc).setRegistryName(loc);
     }
 
     @SubscribeEvent
