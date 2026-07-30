eae931ac3482b8a0e5e30cf7bfcbf35064ff2e84
zzggxx09-byte <zzggxx09@gmail.com>
2026-07-30 13:13:20 +0300

    Update EntityAnya.java

--- src/main/java/com/anyamod/entity/EntityAnya.java
+++ src/main/java/com/anyamod/entity/EntityAnya.java
@@ -1,5 +1,6 @@
 package com.anyamod.entity;
 
+import com.anyamod.entity.ai.EntityAICounterAttack;
 import com.anyamod.entity.ai.EntityAISeekShelterFromRain;
 import com.anyamod.init.ModSounds;
 import net.minecraft.entity.EntityCreature;
@@ -24,26 +25,19 @@ public class EntityAnya extends EntityCreature {
     @Override
     protected void initEntityAI() {
         this.tasks.addTask(0, new EntityAISwimming(this));
-        this.tasks.addTask(1, new EntityAIAttackMelee(this, 1.2D, true));
+        this.tasks.addTask(1, new EntityAICounterAttack(this));
         this.tasks.addTask(2, new EntityAIAvoidEntity<>(this, EntityMob.class, 8.0F, 1.0D, 1.2D));
         this.tasks.addTask(3, new EntityAISeekShelterFromRain(this, 1.0D));
         this.tasks.addTask(4, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
         this.tasks.addTask(5, new EntityAIWanderAvoidWater(this, 0.6D));
         this.tasks.addTask(6, new EntityAILookIdle(this));
-
-        this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true));
-        this.targetTasks.addTask(2, new EntityAINearestAttackableTarget<>(this, EntityMob.class, true));
+        // targetTasks навмисно порожній - вона не переслідує, лише контратакує впритул
     }
 
     @Override
     protected void applyEntityAttributes() {
         super.applyEntityAttributes();
         this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.25D);
-
-        // Радіус, у межах якого вона взагалі "помічає" мобів для атаки (близька дистанція)
-        this.getEntityAttribute(SharedMonsterAttributes.FOLLOW_RANGE).setBaseValue(4.0D);
-
-        // ATTACK_DAMAGE не зареєстрований базовим EntityCreature - без цього краш при атаці
         this.getAttributeMap().registerAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(2.0D);
     }
 
