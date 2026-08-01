9958cd48d0cbb9770b9a44819d0759446b181be2
zzggxx09-byte <zzggxx09@gmail.com>
2026-08-01 16:01:05 +0300

    Update AnyaRespawnData.java

--- src/main/java/com/anyamod/data/AnyaRespawnData.java
+++ src/main/java/com/anyamod/data/AnyaRespawnData.java
@@ -6,14 +6,29 @@ import net.minecraft.world.World;
 import net.minecraft.world.storage.MapStorage;
 import net.minecraft.world.storage.WorldSavedData;
 
+/**
+ * Система респавну Anya з життами
+ * 
+ * СИСТЕМА ЖИТТІВ:
+ * - Anya стартує з 5 життів
+ * - При смерті: livesCount -= 1
+ * - Якщо livesCount = 0: смерть назавжди
+ * - Кожен новий день: +1 life (максимум 5)
+ */
 public class AnyaRespawnData extends WorldSavedData {
 
     private static final String DATA_NAME = "anyamod_anya_respawn";
+    private static final int MAX_LIVES = 5;
 
     private BlockPos homePos;
     private boolean hasHome;
     private boolean pendingRespawn;
     private int respawnTicksLeft;
+    
+    // НОВЕ: Система життів
+    private int livesCount = MAX_LIVES;           // Поточне кількість життів (1-5)
+    private boolean isDeadForever = false;         // Чи вона вмерла назавжди?
+    private long lastDayLiveWasGiven = -1;         // День коли останній раз дали life
 
     public AnyaRespawnData(String name) {
         super(name);
@@ -29,6 +44,8 @@ public class AnyaRespawnData extends WorldSavedData {
         return data;
     }
 
+    // ==================== ДОМУ ====================
+    
     public void setHome(BlockPos pos) {
         this.homePos = pos;
         this.hasHome = true;
@@ -43,6 +60,8 @@ public class AnyaRespawnData extends WorldSavedData {
         return this.homePos;
     }
 
+    // ==================== РЕСПАВН ====================
+    
     public void startRespawnCountdown(int ticks) {
         this.pendingRespawn = true;
         this.respawnTicksLeft = ticks;
@@ -66,14 +85,91 @@ public class AnyaRespawnData extends WorldSavedData {
         return false;
     }
 
+    // ==================== СИСТЕМА ЖИТТІВ ====================
+    
+    /**
+     * Отримати поточне кількість життів (1-5)
+     */
+    public int getLives() {
+        return this.livesCount;
+    }
+
+    /**
+     * Позбавити одного життя при смерті
+     * Повертає true якщо вона ще живе
+     */
+    public boolean loseLife() {
+        if (this.isDeadForever) {
+            return false; // Вже мертва
+        }
+
+        this.livesCount--;
+        
+        if (this.livesCount <= 0) {
+            // СМЕРТЬ НАЗАВЖДИ!
+            this.isDeadForever = true;
+            this.livesCount = 0;
+        }
+        
+        this.markDirty();
+        return this.livesCount > 0;
+    }
+
+    /**
+     * Чи вона вмерла назавжди?
+     */
+    public boolean isDeadForever() {
+        return this.isDeadForever;
+    }
+
+    /**
+     * Дати +1 life при новому дні
+     * Повертає true якщо дали life
+     */
+    public boolean tryGiveLifeForNewDay(long worldTime) {
+        // Визначаємо поточний день (1 день = 24000 тиків)
+        long currentDay = worldTime / 24000L;
+        
+        // Перевіряємо чи це новий день і чи можна дати life
+        if (currentDay > this.lastDayLiveWasGiven && this.livesCount < MAX_LIVES) {
+            this.livesCount++;
+            this.lastDayLiveWasGiven = currentDay;
+            this.markDirty();
+            return true;
+        }
+        
+        return false;
+    }
+
+    /**
+     * Отримати максимальне кількість життів
+     */
+    public int getMaxLives() {
+        return MAX_LIVES;
+    }
+
+    // ==================== NBT ЗБЕРЕЖЕННЯ ====================
+    
     @Override
     public void readFromNBT(NBTTagCompound nbt) {
         this.hasHome = nbt.getBoolean("hasHome");
         if (this.hasHome) {
-            this.homePos = new BlockPos(nbt.getInteger("homeX"), nbt.getInteger("homeY"), nbt.getInteger("homeZ"));
+            this.homePos = new BlockPos(
+                nbt.getInteger("homeX"),
+                nbt.getInteger("homeY"),
+                nbt.getInteger("homeZ")
+            );
         }
         this.pendingRespawn = nbt.getBoolean("pendingRespawn");
         this.respawnTicksLeft = nbt.getInteger("respawnTicksLeft");
+        
+        // НОВЕ: Життя
+        this.livesCount = nbt.getInteger("livesCount");
+        if (this.livesCount <= 0) this.livesCount = 0;
+        if (this.livesCount > MAX_LIVES) this.livesCount = MAX_LIVES;
+        
+        this.isDeadForever = nbt.getBoolean("isDeadForever");
+        this.lastDayLiveWasGiven = nbt.getLong("lastDayLiveWasGiven");
     }
 
     @Override
@@ -86,6 +182,12 @@ public class AnyaRespawnData extends WorldSavedData {
         }
         compound.setBoolean("pendingRespawn", this.pendingRespawn);
         compound.setInteger("respawnTicksLeft", this.respawnTicksLeft);
+        
+        // НОВЕ: Життя
+        compound.setInteger("livesCount", this.livesCount);
+        compound.setBoolean("isDeadForever", this.isDeadForever);
+        compound.setLong("lastDayLiveWasGiven", this.lastDayLiveWasGiven);
+        
         return compound;
     }
 }
