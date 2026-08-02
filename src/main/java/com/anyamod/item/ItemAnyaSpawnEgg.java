364b3e7259614acb2fb5ec27b5101f1e83266580
zzggxx09-byte <zzggxx09@gmail.com>
2026-08-02 12:20:46 +0300

    Update ItemAnyaSpawnEgg.java

--- src/main/java/com/anyamod/item/ItemAnyaSpawnEgg.java
+++ src/main/java/com/anyamod/item/ItemAnyaSpawnEgg.java
@@ -1,6 +1,5 @@
 package com.anyamod.item;
 
-import com.anyamod.data.AnyaRespawnData;
 import com.anyamod.entity.EntityAnya;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
@@ -20,9 +19,11 @@ import net.minecraft.world.World;
  * Яйце спавну Anya
  * 
  * При використанні:
- * 1. Спавниш Anya
+ * 1. Спавниш НОВУ Anya
  * 2. Встановлюється точка дому (для респавну)
- * 3. Anya отримує 5 живів
+ * 3. Anya отримує 5 живів (із NBT)
+ * 
+ * ВАЖЛИВО: Кожна Anya - НЕЗАЛЕЖНА! Власний livesCount, homePos!
  */
 public class ItemAnyaSpawnEgg extends Item {
 
@@ -51,40 +52,40 @@ public class ItemAnyaSpawnEgg extends Item {
         Block block = iblockstate.getBlock();
 
         BlockPos spawnPos = pos.offset(facing);
-        double extraY = 0.0D;
 
-        // Спавниш саму Anya
+        // Спавниш НОВУ Anya
         EntityAnya entity = new EntityAnya(worldIn);
         entity.setLocationAndAngles(
                 spawnPos.getX() + 0.5D,
-                spawnPos.getY() + extraY,
+                spawnPos.getY(),
                 spawnPos.getZ() + 0.5D,
                 MathHelper.wrapDegrees(worldIn.rand.nextFloat() * 360.0F),
                 0.0F
         );
+        
+        // ========== ІНІЦІАЛІЗАЦІЯ ЦІЄЇ ANYA ==========
+        
+        // Встановлюємо дім для ЦІЄЇ Anya
+        entity.setHome(spawnPos);
+        
+        // livesCount встановлюється в NBT при читанні (за замовчуванням 5)
+        // Нічого робити не потрібно - в конструкторі EntityAnya це вже 5
+        
+        // Спавниш entity у світ
         worldIn.spawnEntity(entity);
-
-        // ========== ІНІЦІАЛІЗАЦІЯ ЖИТІВ ==========
         
-        AnyaRespawnData respawnData = AnyaRespawnData.get(worldIn);
-        
-        // Встановлюємо дім (точка респавну)
-        respawnData.setHome(spawnPos);
-        
-        // Anya отримує 5 живів (максимум)
-        // Життя вже стартують з 5 за замовчуванням, але можна явно встановити
-        int startLives = respawnData.getMaxLives();
+        int startLives = entity.getMaxLives();
         
         // Broadcast в чат
         String spawnMessage = TextFormatting.GREEN 
-            + "✓ Аня спавнена! ❤ " + startLives + "/" + startLives + " живів"
+            + "✓ Аня спавнена! ❤ " + startLives + "/" + startLives + " живій"
             + TextFormatting.RESET;
         
         worldIn.getPlayers(EntityPlayer.class, p -> true)
                 .forEach(p -> p.sendMessage(new TextComponentString(spawnMessage)));
         
-        System.out.println("[AnyaMod] Anya спавнена з домом в " + spawnPos);
-        System.out.println("[AnyaMod] Anya має " + startLives + " живів");
+        System.out.println("[AnyaMod] Anya (ID: " + entity.getEntityId() + ") спавнена з домом в " + spawnPos);
+        System.out.println("[AnyaMod] Anya має " + startLives + " живій");
 
         // Видалити яйце зі стеку
         itemstack.shrink(1);
