29983ad56f9e0230dda6b3bb24af185ad312a0b3
zzggxx09-byte <zzggxx09@gmail.com>
2026-08-04 15:09:42 +0300

    Update EntityAnya.java

--- src/main/java/com/anyamod/entity/EntityAnya.java
+++ src/main/java/com/anyamod/entity/EntityAnya.java
@@ -66,17 +66,17 @@ public class EntityAnya extends EntityCreature {
     // ==================== AI ІНІЦІАЛІЗАЦІЯ ====================
 
     @Override
-    protected void initEntityAI() {
-        this.tasks.addTask(0, new EntityAISwimming(this));
-        this.tasks.addTask(1, new EntityAICounterAttack(this));
-        this.tasks.addTask(2, new EntityAIAvoidEntity<>(this, EntityMob.class, 8.0F, 1.0D, 1.2D));
-        this.tasks.addTask(3, new EntityAIEyeContact(this));
-        this.tasks.addTask(4, new EntityAISeekShelterFromRain(this, 1.0D));
-        this.tasks.addTask(5, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
-        this.tasks.addTask(6, new EntityAIWanderAvoidWater(this, 1.0D));
-        this.tasks.addTask(7, new EntityAILookIdle(this));
-        // targetTasks навмисно порожній - контр-атака в AI
-    }
+protected void initEntityAI() {
+    this.tasks.addTask(0, new EntityAISwimming(this));
+    this.tasks.addTask(1, new com.anyamod.entity.ai.EntityAIFreezeForGui(this));
+    this.tasks.addTask(2, new EntityAICounterAttack(this));
+    this.tasks.addTask(3, new EntityAIAvoidEntity<>(this, EntityMob.class, 8.0F, 1.0D, 1.2D));
+    this.tasks.addTask(4, new EntityAIEyeContact(this));
+    this.tasks.addTask(5, new EntityAISeekShelterFromRain(this, 1.0D));
+    this.tasks.addTask(6, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
+    this.tasks.addTask(7, new EntityAIWanderAvoidWater(this, 1.0D));
+    this.tasks.addTask(8, new EntityAILookIdle(this));
+}
 
     @Override
     protected void applyEntityAttributes() {
