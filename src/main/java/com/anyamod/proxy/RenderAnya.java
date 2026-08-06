64a3b3376c3b2a5fecc83b155c875a3a2ddde4f8
zzggxx09-byte <zzggxx09@gmail.com>
2026-08-06 14:08:16 +0300

    Update RenderAnya.java

--- src/main/java/com/anyamod/proxy/RenderAnya.java
+++ src/main/java/com/anyamod/proxy/RenderAnya.java
@@ -2,24 +2,14 @@ package com.anyamod.proxy;
 
 import com.anyamod.AnyaMod;
 import com.anyamod.entity.EntityAnya;
-import com.anyamod.entity.ModelAnyaHD;
-import net.minecraft.client.renderer.entity.RenderLiving;
+import com.anyamod.entity.ModelAnyaGeo;
 import net.minecraft.client.renderer.entity.RenderManager;
-import net.minecraft.util.ResourceLocation;
+import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
 
-public class RenderAnya extends RenderLiving<EntityAnya> {
-
-    private static final ResourceLocation TEXTURE =
-            new ResourceLocation(AnyaMod.MODID, "textures/entity/anya_skin.png");
+public class RenderAnya extends GeoEntityRenderer<EntityAnya> {
 
     public RenderAnya(RenderManager renderManager) {
-        super(renderManager, new ModelAnyaHD(), 0.5F); // 0.5F тут - це радіус тіні на землі, не масштаб моделі
+        super(renderManager, new ModelAnyaGeo());
+        this.shadowSize = 0.5F;
     }
-
-    @Override
-    protected ResourceLocation getEntityTexture(EntityAnya entity) {
-        return TEXTURE;
-    }
-
-    // preRenderCallback більше не перевизначаємо - scale тепер робить сама модель.
 }
