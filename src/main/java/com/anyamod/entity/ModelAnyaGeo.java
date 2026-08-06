ad7351005a1d577bf4dc528aeea3b2f7e1d0e893
zzggxx09-byte <zzggxx09@gmail.com>
2026-08-06 15:11:39 +0300

    Update ModelAnyaGeo.java

--- src/main/java/com/anyamod/entity/ModelAnyaGeo.java
+++ src/main/java/com/anyamod/entity/ModelAnyaGeo.java
@@ -1,6 +1,7 @@
 package com.anyamod.entity;
 
 import net.minecraft.util.ResourceLocation;
+import software.bernie.geckolib3.geo.render.built.GeoBone;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
 
 public class ModelAnyaGeo extends AnimatedGeoModel<EntityAnya> {
@@ -18,5 +19,19 @@ public class ModelAnyaGeo extends AnimatedGeoModel<EntityAnya> {
     @Override
     public ResourceLocation getAnimationFileLocation(EntityAnya animatable) {
         return new ResourceLocation("anyamod", "animations/anya.animation.json");
+    }
+
+    @Override
+    public void setLivingAnimations(EntityAnya entity, Integer uniqueID, software.bernie.geckolib3.core.event.predicate.AnimationEvent customPredicate) {
+        super.setLivingAnimations(entity, uniqueID, customPredicate);
+
+        // ДОДАНО: окремий поворот кістки "Head" - голова дивиться на гравця,
+        // тіло лишається повернутим за напрямком руху/AI.
+        GeoBone head = this.getAnimationProcessor().getBone("Head");
+        if (head != null) {
+            head.setRotationZ(0);
+            head.setRotationY((float) Math.toRadians(entity.rotationYawHead - entity.renderYawOffset));
+            head.setRotationX((float) Math.toRadians(entity.rotationPitch));
+        }
     }
 }
