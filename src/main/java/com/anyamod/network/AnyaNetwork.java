80d2c1c82c1f3ffa819a98fc455b48d5a051ad43
zzggxx09-byte <zzggxx09@gmail.com>
2026-08-08 10:09:45 +0300

    Update AnyaNetwork.java

--- src/main/java/com/anyamod/network/AnyaNetwork.java
+++ src/main/java/com/anyamod/network/AnyaNetwork.java
@@ -11,5 +11,8 @@ public class AnyaNetwork {
     public static void init() {
         CHANNEL = NetworkRegistry.INSTANCE.newSimpleChannel("anyamod_channel");
         CHANNEL.registerMessage(PacketAnyaGuiState.Handler.class, PacketAnyaGuiState.class, 0, Side.SERVER);
+        CHANNEL.registerMessage(PacketGiveItemToAnya.Handler.class, PacketGiveItemToAnya.class, 1, Side.SERVER);
+        CHANNEL.registerMessage(PacketDropItemFromAnya.Handler.class, PacketDropItemFromAnya.class, 2, Side.SERVER);
+        CHANNEL.registerMessage(PacketSyncAnyaInventory.Handler.class, PacketSyncAnyaInventory.class, 3, Side.CLIENT);
     }
-    }
+}
